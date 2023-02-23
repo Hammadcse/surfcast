744be67e6dc7d94e1dc41e58a50db069afe5f506
hammadcse <hammadrahman162@gmail.com>
2023-02-23 21:41:45 +0600

    second page design

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+$('.team_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoPlay: true,
+    dots:false,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+})
